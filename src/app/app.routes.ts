7036ad3bf6f37ab3b24e55e04d909bd21e057e7e
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2024-06-24 22:28:05 -0300

    feat: add favorites page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CharactersComponent } from './pages/characters/characters.component';
 import { EpisodesComponent } from './pages/episodes/episodes.component';
 import { LocationsComponent } from './pages/locations/locations.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
 export const routes: Routes = [
   { path: 'homepage', component: HomePageComponent },
   { path: 'characters', component: CharactersComponent },
   { path: 'episodes', component: EpisodesComponent },
   { path: 'locations', component: LocationsComponent },
+  { path: 'favorites', component: FavoritesComponent },
 ];
